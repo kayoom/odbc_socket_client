54ad19ff22178a23666e312f5ccdd095a911261e
Marian Theisen <marian@cice-online.net>
2010-12-15 16:22:28 +0100

    starting to collect errors as they appear

---
+++ lib/odbc_socket_client/ado_error.rb
@@ -0,0 +1,9 @@
+module OdbcSocketClient
+  class AdoError < Exception
+    
+  end
+  
+  class TableLockedError < AdoError
+    MATCH = /Table '(.*)' is exclusively locked by user '(.*)' on machine '(.*)'/
+  end
+end
