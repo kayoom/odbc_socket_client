33b92c9baf51218ffd654486853484d436a57ede
Marian Theisen <marian@cice-online.net>
2012-07-31 11:13:26 +0200

    allow sql strings

--- lib/active_record/connection_adapters/odbc_socket_adapter.rb
+++ lib/active_record/connection_adapters/odbc_socket_adapter.rb
@@ -5,16 +5,16 @@ module ActiveRecord
     def instantiate record
       super remap(record)
     end
-    
+
     def remap_sql sql
       # Hack to support Jet SQL's idiotic TOP 10 syntax
       if limit = sql.match(/LIMIT (\d+)/)
         limit_clause, limit = limit.to_a[0,2]
-    
+
         sql.sub! limit_clause, ''
         sql.sub! 'SELECT', "SELECT TOP #{limit}"
       end
-  
+
       # Column mapping hack, i should've based this on DataMapper...
       sql.gsub! /#{table_name}\.([a-zA-Z0-9_]+)/ do |match|
         column_name = $1
@@ -26,21 +26,22 @@ module ActiveRecord
           "#{table_name}.#{column_name}"
         end
       end
-  
+
       sql
     end
-    
+
     def find_by_sql sql, *args
-      super remap_sql(sql.to_sql), *args
+      sql = String === sql ? sql : sql.to_sql
+      super remap_sql(sql), *args
     end
-    
+
     def count_by_sql sql
       super remap_sql(sql)
     end
-    
+
     def remap record
       attributes = {}
-      
+
       record.each do |column_name, value|
         if column = column_mapping[column_name.to_s]
           attributes[column.name.to_s] = value
@@ -48,17 +49,17 @@ module ActiveRecord
           attributes[column_name.to_s] = value
         end
       end
-      
+
       attributes
     end
   end
-  
+
   class Base
     class ColumnMapper
       def initialize mapping
         @mapping = mapping
       end
-      
+
       %w(string integer float decimal datetime date timestamp time binary boolean text).each do |type|
         define_method type do |column_name, attribute_name|
           @mapping[column_name.to_s] = ConnectionAdapters::MappedColumn.new(attribute_name.to_s, nil, type).tap do |c|
@@ -67,36 +68,36 @@ module ActiveRecord
         end
       end
     end
-    
-    
+
+
     class_attribute :column_mapping
-    
+
     class << self
       def odbc_socket_connection config
         ConnectionAdapters::OdbcSocketAdapter.new config, logger
       end
-      
+
       def map_primary_key name
         connection.set_primary_key self, name
-        
+
         set_primary_key name
       end
-      
+
       def map_columns options = {}, &block
         options = {
           :default_scope => true
         }.merge(options)
-        
+
         self.column_mapping = {}
         block.call ColumnMapper.new(self.column_mapping)
-        
+
         connection.table_columns[table_name.to_s] = column_mapping.values
-        
+
         default_scope select(column_mapping.keys) if options[:default_scope]
-        
+
         extend ColumnMapping
       end
-      
+
     end
   end
 end
@@ -107,9 +108,9 @@ module ActiveRecord
       module Format
         MS_ACCESS_DATETIME = /\A(\d{4})-(\d\d)-(\d\d) (\d\d?):(\d\d?):(\d\d?)(\.\d+)?\z/
       end
-      
+
       attr_accessor :sql_column_name
-      
+
       def self.fast_string_to_time string
         if string =~ Format::MS_ACCESS_DATETIME
           microsec = ($7.to_f * 1_000_000).to_i
@@ -117,39 +118,39 @@ module ActiveRecord
         end
       end
     end
-    
+
     class OdbcSocketAdapter < AbstractAdapter
       attr_accessor :table_columns, :primary_keys
       ADAPTER_NAME = 'OdbcSocket'.freeze
       TABLES_QUERY = "SELECT * FROM MSysObjects WHERE (MSysObjects.Type = 1 AND left(MSysObjects.Name, 4) <> 'MSys');".freeze
       TABLE_NAME_COLUMN = :Name
-    
+
       def initialize config, logger = nil
         @client = OdbcSocketClient::Client.new config, logger
         super @client, logger
-        
+
         @table_columns = {}
         @primary_keys = {}
       end
-      
+
       def tables
         @tables ||= load_tables
-      end      
-      
+      end
+
       def table_exists? table_name
         tables.include? table_name.to_s
       end
-      
+
       def set_primary_key table, key
         @primary_keys ||= {}
-        
+
         @primary_keys[table.table_name.to_s] = key.to_s
       end
-      
+
       def primary_key table_name
         @primary_keys[table_name.to_s]
       end
-      
+
       def quoted_true
         "True"
       end
@@ -157,11 +158,11 @@ module ActiveRecord
       def quoted_false
         "False"
       end
-    
+
       def adapter_name
         ADAPTER_NAME
-      end    
-      
+      end
+
       def quote(value, column = nil)
         # records are quoted as their primary key
         return value.quoted_id if value.respond_to?(:quoted_id)
@@ -191,29 +192,29 @@ module ActiveRecord
             end
         end
       end
-      
+
       def quoted_date value
         "##{value.strftime("%Y-%m-%d")}#"
       end
-      
+
       def columns table_name, whatever = nil
         @table_columns[table_name.to_s]
       end
-      
+
       def select sql, name = nil, binds = []
         result = @client.execute_query(sql)
-        
+
         raise result.error if result.error?
-        
+
         result.rows
       end
-      
+
       protected
-      def load_tables        
+      def load_tables
         @client.execute_query(TABLES_QUERY).rows.map do |table|
           table[TABLE_NAME_COLUMN]
         end
       end
-    end    
-  end  
+    end
+  end
 end
--- lib/odbc_socket_client/result.rb
+++ lib/odbc_socket_client/result.rb
@@ -5,34 +5,34 @@ require 'iconv'
 module OdbcSocketClient
   class Result
     attr_accessor :status, :rows, :columns, :error
-    
+
     def initialize xml_result
       @status = 'unknown'
       @rows = []
-      
+
       parse iconv(xml_result)
     end
-    
+
     def success?
       status.to_s == 'success'
     end
-    
+
     def error?
       status.to_s == 'failure'
-    end    
-    
+    end
+
     protected
     def iconv xml_result
       Iconv.conv 'UTF-8', 'LATIN-9', xml_result
     end
-    
+
     def parse xml_result
       doc = Nokogiri::XML.parse xml_result
 
       root_element = doc.xpath('/result').first
-      
+
       @status = root_element.attr('state').to_s
-      
+
       case @status
       when 'success'
         parse_rows root_element
@@ -40,51 +40,51 @@ module OdbcSocketClient
         parse_error root_element
       end
     end
-    
+
     def parse_error element
       @error = element.xpath('./error').first.try :text
     end
-    
+
     def parse_rows element
       @rows = []
-      
+
       row_nodes = element.element_children
-      
+
       if first_row = row_nodes.first
         @columns = parse_columns first_row
       else
         return
       end
-      
+
       row_nodes.each do |row|
         parse_row row
       end
     end
-    
+
     def parse_row row
       current_row = {}
       column_number = 0
-      
+
       @rows << current_row
-      
+
       column_nodes = row.element_children
-      
+
       column_nodes.each do |column|
         current_row[@columns[column_number]] = column.text
         column_number += 1
       end
     end
-    
+
     def parse_columns row
       column_nodes = row.element_children
-      
+
       [].tap do |columns|
         column_nodes.each do |column|
           name = column.attr 'name'
-          
+
           columns << name.to_sym
-        end        
-      end      
+        end
+      end
     end
   end
 end
