64f7e67792b5d1ec89de9722d80d7eb693ad0781
Marian Theisen <marian@cice-online.net>
2012-08-09 08:37:42 +0200

    enabling query timeout

--- lib/odbc_socket_client/client.rb
+++ lib/odbc_socket_client/client.rb
@@ -7,33 +7,33 @@ require 'odbc_socket_client/connection_string'
 
 module OdbcSocketClient
   class Client
-    
-    
+
+
     def initialize config, logger
       config = config.symbolize_keys
-      
+
       @connection_string = ConnectionString.build config
       @host = config[:host]
       @port = config[:port].to_i || 9628
       @logger = logger
     end
-    
+
     def build_request query
       Request.new query, @connection_string
     end
-    
+
     def connection
       @connection ||= Connection.new @host, @port
     end
-    
+
     def execute_query query
       execute build_request(query)
     end
-    
+
     def execute request
       connection.socket do |s|
         s.print request.to_xml
-        
+
         Result.new s.read
       end
     end
